e8a65af4586f97e085f319cdb5e6bd2123443237
Balabharath2630 <balabharath2630@gmail.com>
2026-04-28 07:28:37 +0530

    Fixed CORS headers issue

--- src/main/java/com/indiaexplorer/config/WebConfig.java
+++ src/main/java/com/indiaexplorer/config/WebConfig.java
@@ -8,11 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-	    registry.addMapping("/**")
-	            .allowedOrigins("https://indian-heritage-frontend-team-5-sec55.vercel.app")
-	            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-	            .allowedHeaders("*")
-	            .allowCredentials(true);
-	}
+public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins("https://indian-heritage-frontend-team-5-sec55.vercel.app")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+}
 }
